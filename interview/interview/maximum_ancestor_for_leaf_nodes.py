50907f4d01cbd5dc1f0d85227a50679a0d3797cc
Denise Avila <denisefavila@gmail.com>
2024-11-04 10:05:34 -0300

    feat: add description

--- interview/interview/maximum_ancestor_for_leaf_nodes.py
+++ interview/interview/maximum_ancestor_for_leaf_nodes.py
@@ -2,6 +2,25 @@ from interview.interview.general.tree_node import TreeNode
 
 
 def max_ancestor_for_leaf_nodes(root: TreeNode):
+    """
+
+    Given a tree, calculate the maximum ancestor for all the leaf nodes.
+    Maximum ancestor of a leaf node is the maximum of its ancestors and the leaf itself.
+
+    Example:
+
+            4
+          /   \
+         5     3
+        /     / \
+       1     2   6
+
+    Output -
+    1: 5
+    2: 4
+    6: 6
+
+    """
     result = {}
 
     def explore(node: TreeNode, current_max: int):
