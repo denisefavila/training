8a3ca2822d86aafba0cc450739b7cb8d159f2a19
Denise Avila <denisefavila@gmail.com>
2024-11-05 16:53:04 -0300

    feat: add dp solution

--- interview/interview/longest_increasing_subsequence.py
+++ interview/interview/longest_increasing_subsequence.py
@@ -3,6 +3,21 @@ from typing import List
 
 
 def longest_increasing_subsequence(nums: List[int]) -> int:
+    if not nums:
+        return 0
+
+    n = len(nums)
+
+    dp = [1] * n
+    for i in range(1, n):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+
+def longest_increasing_subsequence_memo(nums: List[int]) -> int:
     @lru_cache(None)
     def explore(idx):
         if idx >= len(nums):
